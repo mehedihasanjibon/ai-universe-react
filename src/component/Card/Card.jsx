bcea88f568ff1272af46e638cb167783086cf2fe
Mehedi Hasan <mhjibon.info@gmail.com>
2023-07-13 00:50:15 +0600

    single data set and card load data done

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,22 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import SingleData from '../SingleData/SingleData';
 
-const Card = () => {
+const Card = (props) => {
     const [data, setData] = useState([]);
 
     useEffect(()=> {
         const loadData = async() => {
             const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
             const value = await res.json();
-            console.log(value.data.tools);
+            // console.log(value.data.tools);
+            setData(value.data.tools);
         }
         loadData();
     },[])
     return (
-        <div>
-            {
-                data.map(singleData => console.log(value))
-            }
-        </div>
+        <>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:px-12 my-6'>
+                {/* {
+                    data.map(singleData => {
+                        console.log(singleData);
+                        return <SingleData
+                            singleData={singleData}
+                        ></SingleData>
+                    })
+                } */}
+                {
+                    data.map((singleData) => (
+                        <SingleData
+                            key={singleData.id}
+                            singleData={singleData}
+                        ></SingleData>
+                    ))
+                }
+            </div>
+        </>
     );
 };
 
